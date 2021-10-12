d494d746a8718409bc4d0b4d836638f7a93f1939
wencakisa <wencakisa@gmail.com>
2021-10-12 16:33:26 +0300

    Code until "Section 6" (components layout & basic functionality)

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Test recipe', 'description', 'https://picsum.photos/700')
+  ];
+}
